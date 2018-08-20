bfab824cfb5ac81d4bf2afb0fc1dca107d9e8b2f
Richard <cyanyao@live.com>
2018-08-20 15:16:49 +0800

    add children support

--- src/components/title.tsx
+++ src/components/title.tsx
@@ -2,13 +2,31 @@ import * as React from 'react';
 
 
 interface Props {
-  title: string | JSX.Element
+  title?: string,
+  children?: JSX.Element
 }
 
 function Title(props: Props) {
-  return (
-    <h1 className="title">{props.title}</h1>
-  )
+
+  const { title, children } = props;
+
+  if (title !== undefined) {
+    return (
+      <h1 className="title">{props.title}</h1>
+    )
+  } else if (children !== undefined) {
+    return (
+      <h1>
+        {props.children}
+      </h1>
+    )
+  } else {
+    return (
+      <h1>
+        No value passed
+      </h1>
+    )
+  }
 }
 
 export default Title;
