2c48dadf630df20360fafb16e155ab899a0c2fc8
Rick <cyanyao@live.com>
2018-08-27 23:49:09 +0800

    remove react-spring because not meet needs

--- src/Contents.tsx
+++ src/Contents.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { Route } from 'react-router-dom';
-import { config, Spring } from 'react-spring';
 
 class Contents extends React.Component {
   public render() {
@@ -8,14 +7,7 @@ class Contents extends React.Component {
     return (
       <Route path="/articles">
         {(props: { match: boolean }) =>
-          <Spring
-            config={config.gentle}
-            from={{ opacity: props.match ? 0 : 1 }}
-            to={{ opacity: props.match ? 1 : 0 }}>
-            {styles => 
-              <h1 style={styles}>Test</h1>
-            }
-          </Spring>
+          props.match && <h1>Test</h1>
         }
       </Route>
     )
