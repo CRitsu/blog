79f37eb06aefa6e99d1dd139f4e4468bed6da563
Richard <cyanyao@live.com>
2018-09-05 18:14:17 +0800

    update header of lists

--- src/Lists.tsx
+++ src/Lists.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { translate } from 'react-i18next';
 import { Link, Route } from 'react-router-dom';
 import { Square } from './components';
-import { size } from './constants';
+import { CATEGORIES, LATEST, size } from './constants';
 import { Articles } from './type';
 import { formatDate } from './utils';
 
@@ -13,10 +13,31 @@ interface Props {
   t: (p: string) => string
 }
 
-class Lists extends React.Component<Props, object> {
+interface State {
+  activeTab: number
+}
+
+class Lists extends React.Component<Props, State> {
+
+  public state = {
+    // 1: latest; 2: categories
+    activeTab: LATEST
+  }
+
+  public constructor(props: Props) {
+    super(props);
+    this.handleSwitchTabs = this.handleSwitchTabs.bind(this);
+  }
+
+  public handleSwitchTabs(tab: number) {
+    this.setState({activeTab: tab});
+  }
+
   public render() {
 
     const { lists, width, t } = this.props;
+
+    const { activeTab } = this.state;
 
     const listWidth = width > size.LIST_MAX_WIDTH ? size.LIST_MAX_WIDTH : width;
 
@@ -58,6 +79,17 @@ class Lists extends React.Component<Props, object> {
       return match ? 'lists-wrapper matched' : 'lists-wrapper';
     }
 
+    const calculateActiveTabClasses = (active: boolean) => {
+      if (active) {
+        return 'tabs active';
+      } else {
+        return 'tabs';
+      }
+    }
+
+    const switch2Latest = () => this.handleSwitchTabs(LATEST);
+    const switch2Categories = () => this.handleSwitchTabs(CATEGORIES);
+
     return (
       <Route path="/" exact={true}>
         {
@@ -74,7 +106,22 @@ class Lists extends React.Component<Props, object> {
                 }}>
                 <div className="lists">
                   <div className="header">
-                    <input className="search" placeholder={t('Search')} />
+                    <div className="sign">Time flies so fast.</div>
+                    <div className="sub-bar">
+                      <div className="categories">
+                        <div onClick={switch2Latest}
+                          className={calculateActiveTabClasses(activeTab === LATEST)}>
+                          {t('latest')}
+                        </div>
+                        <div onClick={switch2Categories}
+                          className={calculateActiveTabClasses(activeTab === CATEGORIES)}>
+                          {t('categories')}
+                        </div>
+                      </div>
+                      <div className="search">
+                        <input className="input" placeholder={t('Search')} />
+                      </div>
+                    </div>
                   </div>
                   <EditedList match={props.match} />
                 </div>
