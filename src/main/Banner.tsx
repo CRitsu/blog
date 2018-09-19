36f7440820d2513d68a1c1050bf3156fd8bea412
Richard <cyanyao@live.com>
2018-09-19 18:07:05 +0800

    update banner

--- src/main/Banner.tsx
+++ src/main/Banner.tsx
@@ -1,38 +1,61 @@
 import * as React from 'react';
 import { translate } from 'react-i18next';
-import { Route } from 'react-router-dom';
+import { Link, Route } from 'react-router-dom';
+import { Block } from '../components';
+import { Translate } from '../types';
 
-interface Props {
-  t: (s: string) => string
-}
 
-class Banner extends React.Component<Props, object> {
+class Banner extends React.Component<Translate, object> {
   public render() {
 
+    // import t function for translate
     const { t } = this.props;
 
+    const bgLightBlue = 'bg-light-blue';
+
     const ban = () => (
-        <div className="banner">
-          <div className="nav">
-            <div className="index">index</div>
-            <div className="home">home</div>
-            <div className="buttons">buttons</div>
+      <div className="banner">
+
+        <div className="nav">
+
+          <div className="index">
+            <Block className={bgLightBlue} />
           </div>
-          <div className="main-banner">
-            <div className="logo">{t('RICHARD Z')}</div>
-            <div className="motto">
-              <span className="motto-1st-line">{t('mottoThe1stLine')}</span>
-              <span className="motto-2nd-line">{t('mottoThe2ndLine')}</span>
-              <span className="motto-3rd-line">{t('mottoThe3rdLine')}</span>
-            </div>
-            <div className="social">
-              <span className="github">github</span>
-              <span className="weibo">weibo</span>
-            </div>
-            <div className="categories">categories</div>
-            <div className="arrow">arrow</div>
+
+          <div className="home">
+            <Link to="/">...</Link>
           </div>
+
+          <div className="blocks">
+            <Block className={bgLightBlue} />
+            <Block className={bgLightBlue} />
+            <Block className={bgLightBlue} />
+          </div>
+
         </div>
+
+        <div className="main-banner">
+
+          <div className="logo">{t('RICHARD Z')}</div>
+
+          <div className="motto">
+            <div className="motto-1st-line">{t('mottoThe1stLine')}</div>
+            <div className="motto-2nd-line">{t('mottoThe2ndLine')}</div>
+            <div className="motto-3rd-line">{t('mottoThe3rdLine')}</div>
+          </div>
+
+          <div className="social">
+            <div className="github">github</div>
+            <div className="weibo">weibo</div>
+          </div>
+
+          <div className="categories">categories</div>
+
+        </div>
+
+        <div className="arrow" />
+
+      </div>
     );
 
     return (
