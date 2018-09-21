d410090da9d1584c7f7a4837f0a0a8aae735bb4c
Richard <cyanyao@live.com>
2018-09-22 01:38:46 +0800

    extract Arrow component as a accessory

---
+++ src/main/accessories/NaughtyArrow.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+
+class NaughtyArrow extends React.Component<object> {
+
+  public render() {
+    return (
+      <div className="arrow"><div className="inner-shadow" /></div>
+    )
+  }
+}
+
+export default NaughtyArrow;
