377ec33dfb849c4136f5b0d257952a77ef4ad43c
Richard <cyanyao@live.com>
2018-09-23 21:09:52 +0800

    update arrow component

--- src/main/accessories/NaughtyArrow.tsx
+++ src/main/accessories/NaughtyArrow.tsx
@@ -1,11 +1,28 @@
 import * as React from 'react';
 
 
-class NaughtyArrow extends React.Component<object> {
+class NaughtyArrow extends React.Component<object, object> {
+
+  public position = React.createRef();
+
+  public constructor(props: any) {
+    super(props);
+    this.handleMousemove = this.handleMousemove.bind(this);
+  }
+
+  public handleMousemove(e: MouseEvent) {
+    console.log(this.position);
+  }
+
+  public componentDidMount() {
+    window.addEventListener('mousemove', this.handleMousemove);
+  }
 
   public render() {
     return (
-      <div className="arrow"><div className="inner-shadow" /></div>
+      <div className="dont-touch-me">
+        <div className="arrow"><div className="inner-shadow" /></div>
+      </div>
     )
   }
 }
