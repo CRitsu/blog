ab338c1e4cc1b02281420a3425983052197d4a73
Richard <cyanyao@live.com>
2018-10-08 00:17:00 +0800

    add loading message and css styles

--- src/main/layouts/Lists.tsx
+++ src/main/layouts/Lists.tsx
@@ -3,14 +3,12 @@ import { translate } from 'react-i18next';
 import { Link } from 'react-router-dom';
 import { Block } from '../../components';
 import { LATEST } from '../../constants';
-import { Articles, Translate } from '../../types';
+import { Articles, ListsType, Translate } from '../../types';
 import { formatDate } from '../../utils';
 import ControlBar from '../accessories/ControlBar';
 
 
-interface Props extends Translate {
-  list: Articles[]
-}
+interface Props extends Translate, ListsType { }
 
 interface ActiveTab {
   activeTab: number
@@ -20,29 +18,30 @@ interface ActiveTab {
  * Create articles list.
  * @param props array of articles
  */
-function EditedList(props: { list: Articles[] }) {
-  const lists = props.list;
+function EditedList(props: { list: Articles[], more: string }) {
+  const { list, more } = props;
 
-  const Timestamp = (p: {time: number}) => {
+  const Timestamp = (p: { time: number }) => {
 
     const t = formatDate(p.time).split('\n');
 
     return (
       <div className="timestamp">
-        {t[0]}<br/>{t[1]}
+        {t[0]}<br />{t[1]}
       </div>
     )
   }
 
   return (
     <div className={'list-contents'}>
-      {lists.map(item => (
+      {list.map(item => (
         <div key={item._id} className={`item ${item.category}`}>
           <Block />
           <Timestamp time={item.timestamp} />
           <Link className="item-title" to={'/articles/' + item._id}>{item.title}</Link>
         </div>
       ))}
+      <button className="more">{more}</button>
     </div>
   )
 }
@@ -57,14 +56,25 @@ class Lists extends React.Component<Props, ActiveTab, { store: object }> {
 
   public render() {
 
-    const { list, t } = this.props;
+    const { list, loading, t } = this.props;
     const activeTab = this.state.activeTab;
+
+    const Loading = () => (
+      <div className="loading">
+        <span>{t('loading')}</span>
+        <Block />
+        <Block />
+        <Block />
+      </div>
+    )
 
     return (
       <div className="lists">
         <ControlBar t={t} activeTab={activeTab} />
-        <EditedList list={list} />
-        <button className="more">{t('load more')}</button>
+        {loading
+          ? <Loading />
+          : <EditedList list={list} more={t('load more')} />
+        }
       </div>
     )
   }
