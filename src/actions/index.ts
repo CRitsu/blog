850a29e8adc753d95ffb0949cb093e585cc8cfe8
Richard <cyanyao@live.com>
2018-10-08 00:11:46 +0800

    move actions to reducer folder

--- src/actions/index.ts
+++
@@ -1,6 +0,0 @@
-import * as actions from './actions';
-import * as creators from './creators';
-
-export {
-  actions, creators
-}
---
+++ src/reducers/actions.ts
@@ -0,0 +1,16 @@
+
+export const TEST = 'TEST';
+export type TEST = typeof TEST;
+
+export const FETCH_LISTS = 'FETCH_LISTS';
+export type FETCH_LISTS = typeof FETCH_LISTS;
+
+
+// for start fetching
+export const FETCHING_LIST = 'FETCHING_LIST';
+export type FETCHING_LIST = typeof FETCHING_LIST;
+
+// for complete fetched
+export const FETCHED_LIST = 'FETCHED_LIST';
+export type FETCHED_LIST = typeof FETCHED_LIST;
+
