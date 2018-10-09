35d29ebdd41f23b330ff937ea78bffc763763189
Richard <cyanyao@live.com>
2018-10-09 18:04:53 +0800

    update reducers

--- src/reducers/fetchReducers.ts
+++ src/reducers/fetchReducers.ts
@@ -1,1 +1,20 @@
+import { BaseAction, ListsType } from "../types";
+import { FETCHING_LIST } from "./actions";
+
 // for fetch api relevant reducers
+
+export const lists = (
+    state: ListsType,
+    action: BaseAction
+  ): ListsType => {
+    
+    switch (action.type) {
+      // start fetching action
+      case FETCHING_LIST:
+        return Object.assign({}, state, {loading: true});
+
+      default:
+        return state;
+    }
+
+}
