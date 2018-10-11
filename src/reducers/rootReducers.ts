13e476a7e288e977e1d024ce175b9be36af39ca7
Richard <cyanyao@live.com>
2018-10-11 10:24:01 +0800

    add fetching action handler

--- src/reducers/rootReducers.ts
+++ src/reducers/rootReducers.ts
@@ -1,12 +1,39 @@
-import { ContentsType, ListsType } from '../types';
+import { BaseAction, ContentsType, ListsType } from '../types';
+import { LIST_FETCH_FAILED, LIST_FETCHED, LIST_FETCHING } from './actions';
 
 
-export const lists = (): ListsType => ({
-  isError: false,
-  list: [], 
-  loading: true, 
-});
+export const lists = (
+  state: ListsType = {
+    isError: false,
+    list: [],
+    loading: true,
+  }, action: BaseAction
+): ListsType => {
 
-export const contents = (): ContentsType => ({
-  title: '',
-});
+  switch (action.type) {
+    // start fetching action
+    case LIST_FETCHING:
+      return Object.assign({}, state, { loading: true, isError: false });
+
+    // end fetching action
+    case LIST_FETCHED:
+      return Object.assign({}, state, { list: action.payload.list });
+
+    // fetching failed action
+    case LIST_FETCH_FAILED:
+      return Object.assign({}, state, { loading: false, isError: true });
+
+    default:
+      return state;
+  }
+
+};
+
+export const contents = (
+  state: ContentsType = {
+    title: '',
+  },
+  action: BaseAction
+): ContentsType => {
+  return state;
+}
