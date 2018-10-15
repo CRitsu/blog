5349585de791fa0347370d9d4aaebbfa82814a36
Richard <cyanyao@live.com>
2018-10-15 17:09:24 +0800

    add style trigger to categories tabs

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -5,4 +5,8 @@ export { icons, size };
 
 export const LATEST = 1;
 export const CATEGORIES = 2;
+export const TECH = 21;
+export const MEMO = 22;
+export const PHOTO = 23;
+export const TALK = 24;
 export const TAGS = 3;
--- src/main/accessories/ControlBar.tsx
+++ src/main/accessories/ControlBar.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { CATEGORIES, LATEST, TAGS } from '../../constants';
+import { CATEGORIES, LATEST, MEMO, PHOTO, TAGS, TALK, TECH } from '../../constants';
 import { Translate } from '../../types';
 
 
@@ -50,24 +50,36 @@ class ControlBar extends React.Component<Props, State> {
   }
 
   /**
-   * Handler for scroll event.
-   * 
-   * Set position when scroll over the control bar.
+   * Calculate current position if it should change position.
    */
-  public handleScroll() {
+  public getClassNames() {
     const { cls, bar } = this.state;
 
     // when current scroll position is over the bar
     if (this.getOffsetTop(bar) < window.pageYOffset) {
       // set class name only when current class name is not 'hold'
       if (cls === '') {
-        this.setState({ cls: 'hold' });
+        return 'hold';
       }
     } else {
       // reset class name only when it is not ''
       if (cls === 'hold') {
-        this.setState({ cls: '' });
+        return '';
       }
+    }
+
+    return undefined;
+  }
+
+  /**
+   * Handler for scroll event.
+   * 
+   * Set position when scroll over the control bar.
+   */
+  public handleScroll() {
+    const cls = this.getClassNames();
+    if (cls !== undefined) {
+      this.setState({ cls });
     }
   }
 
@@ -100,6 +112,11 @@ class ControlBar extends React.Component<Props, State> {
    */
   public componentDidMount() {
     window.addEventListener('scroll', this.handleScroll);
+    // fix position when switch active tab
+    const cls = this.getClassNames();
+    if (cls !== undefined) {
+      this.setState({ cls });
+    }
   }
 
   /**
@@ -116,17 +133,43 @@ class ControlBar extends React.Component<Props, State> {
     // edit class name
     const className = 'real-bar ' + cls;
 
+    // for latest/categories/tags
+    const O = 0;
+    // for tech/memo/photo/talk
+    const I = 1;
+
     // edit active tab's class name
-    const applyActive = (name: number) => {
-      if (name === activeTab) {
-        return 'tab active';
+    const applyActive = (name: number, mode = O) => {
+      if (mode === O) {
+        const at = activeTab > 9 ? Math.floor(activeTab / 10) : activeTab;
+        if (name === at) {
+          return 'active';
+        }
+      } else if (mode === I) {
+        const at = activeTab;
+        if (name === at) {
+          return 'cat active';
+        } else {
+          return 'cat';
+        }
+      }
+      return '';
+    };
+
+    const getCategoryMenuClassName = () => {
+      const cn = 'categories-menu';
+      if (Math.floor(activeTab / 10) === CATEGORIES) {
+        return cn + ' active';
       } else {
-        return 'tab';
+        return cn;
       }
     }
 
     const switch2latest = () => onSwitchTab(LATEST);
-    const switch2categories = () => onSwitchTab(CATEGORIES);
+    const switch2tech = () => onSwitchTab(TECH);
+    const switch2memo = () => onSwitchTab(MEMO);
+    const switch2photo = () => onSwitchTab(PHOTO);
+    const switch2talk = () => onSwitchTab(TALK);
     const switch2tags = () => onSwitchTab(TAGS);
 
 
@@ -134,9 +177,9 @@ class ControlBar extends React.Component<Props, State> {
       <div className="control-bar" ref={bar}>
         <div className={className}>
           <div className="tabs">
-            <button className={applyActive(LATEST)} onClick={switch2latest}>{t('latest')}</button>
-            <button className={applyActive(CATEGORIES)} onClick={switch2categories}>{t('categories')}</button>
-            <button className={applyActive(TAGS)} onClick={switch2tags}>{t('tags')}</button>
+            <button className={`tab ${applyActive(LATEST)}`} onClick={switch2latest}>{t('latest')}</button>
+            <button className={`tab ${applyActive(CATEGORIES)}`} onClick={switch2tech}>{t('categories')}</button>
+            <button className={`tab ${applyActive(TAGS)}`} onClick={switch2tags}>{t('tags')}</button>
           </div>
           <div className="search-area">
             <label className="magnifier">
@@ -145,8 +188,11 @@ class ControlBar extends React.Component<Props, State> {
                 onFocus={this.handleOnfocus} onBlur={this.handleOnblur} />
             </label>
           </div>
-          <div className="categories-menu">
-            categories menu
+          <div className={getCategoryMenuClassName()}>
+            <button className={applyActive(TECH, I)} onClick={switch2tech} >{t('tech')}</button>
+            <button className={applyActive(MEMO, I)} onClick={switch2memo} >{t('memo')}</button>
+            <button className={applyActive(PHOTO, I)} onClick={switch2photo} >{t('photo')}</button>
+            <button className={applyActive(TALK, I)} onClick={switch2talk} >{t('talk')}</button>
           </div>
         </div>
       </div>
