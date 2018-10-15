78e475fc65083eb94f027b0eb59fd0236eb94b87
Richard <cyanyao@live.com>
2018-10-16 00:45:56 +0800

    pass dispatch and update links to categories

--- src/main/layouts/Banner.tsx
+++ src/main/layouts/Banner.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import { translate } from 'react-i18next';
+import { Link } from 'react-router-dom';
 import { Block } from '../../components';
 import { blogName, github, weibo } from '../../config';
 import { MEMO_COLOR, PHOTO_COLOR, TALK_COLOR, TECH_COLOR } from '../../constants/colors';
-import { Translate } from '../../types';
+import { CommonType, ReduxDispatch, Translate } from '../../types';
 import NaughtyArrow from '../accessories/NaughtyArrow';
 
 
@@ -11,13 +12,17 @@ interface State {
   home: string;
 }
 
+interface Props extends Translate, ReduxDispatch, CommonType {}
 
-class Banner extends React.Component<Translate, State> {
+
+class Banner extends React.Component<Props, State> {
 
   public render() {
 
     // import t function for translate
-    const { t } = this.props;
+    const { listTopPoint, t, dispatch } = this.props;
+
+    const scroll = () => window.scrollTo(0, listTopPoint);
 
     return (
       <div className="banner">
@@ -39,24 +44,24 @@ class Banner extends React.Component<Translate, State> {
 
           <div className="categories">
             <div className="item">
-              <Block className={TECH_COLOR} />
+              <Link to="/list/tech" onClick={scroll}><Block className={TECH_COLOR} /></Link>
               <div className="category">{t('tech')}</div>
             </div>
             <div className="item">
-              <Block className={MEMO_COLOR} />
+              <Link to="/list/memo" onClick={scroll}><Block className={MEMO_COLOR} /></Link>
               <div className="category">{t('memo')}</div>
             </div>
             <div className="item">
-              <Block className={PHOTO_COLOR} />
+            <Link to="/list/photo" onClick={scroll}><Block className={PHOTO_COLOR} /></Link>
               <div className="category">{t('photo')}</div>
             </div>
             <div className="item">
-              <Block className={TALK_COLOR} />
+            <Link to="/list/talk" onClick={scroll}><Block className={TALK_COLOR} /></Link>
               <div className="category">{t('talk')}</div>
             </div>
           </div>
 
-          <NaughtyArrow />
+          <NaughtyArrow dispatch={dispatch} />
 
         </div>
       </div>
