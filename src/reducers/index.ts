7ff0e99f574168cbf628ac01a5f39361feb4fe23
Richard <cyanyao@live.com>
2018-10-22 00:15:59 +0800

    update reducers template

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -2,10 +2,11 @@ import reduceReducer from 'reduce-reducers';
 import { combineReducers } from 'redux';
 import * as act from './actions';
 import * as actCreators from './creators';
+import normalReducers from './normalReducers';
 import * as rootReducers from './rootReducers';
 
 export const actions = act;
 export const actionCreators = actCreators;
 
 const reducerRoot = combineReducers(rootReducers);
-export const reducers = reduceReducer(reducerRoot);
+export const reducers = reduceReducer(reducerRoot, ...normalReducers);
