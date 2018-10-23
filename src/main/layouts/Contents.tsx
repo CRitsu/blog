96416dde4173b5d697db3e10a5432d38305caaab
Richard <cyanyao@live.com>
2018-10-23 23:20:59 +0800

    update comments styles

--- src/main/layouts/Contents.tsx
+++ src/main/layouts/Contents.tsx
@@ -3,8 +3,9 @@ import 'highlight.js/styles/monokai-sublime.css';
 import * as marked from 'marked';
 import * as React from 'react';
 import { translate } from 'react-i18next';
+import { NORMAL_TIME_FORMAT, SHORT_TIME_FORMAT } from 'src/constants';
 import { fetchArticle } from 'src/reducers/creators';
-import { checkArticle } from 'src/utils';
+import { checkArticle, formatDate } from 'src/utils';
 import { Comments, ContentsType, ReduxDispatch, Translate } from '../../types';
 import Loading from '../accessories/Loading';
 
@@ -56,13 +57,19 @@ class Contents extends React.Component<Props> {
               <h1 className="title">{article.title}</h1>
               <div className="article-info">
                 <span>{article.author}</span>
-                <span>{article.timestamp}</span>
+                <span>{formatDate(article.timestamp, SHORT_TIME_FORMAT)}</span>
               </div>
               <div className="article-content"
                 dangerouslySetInnerHTML={{ __html: marked(article.content) }} />
               <div className="comments">
                 {article.comments.map((comment: Comments) => (
-                  <div className="comment" key={comment.cid}>{comment.body}</div>
+                  <div className="comment" key={comment.cid}>
+                  <div className="head">
+                    <div className="from">{comment.from}</div>
+                    <div className="time">{formatDate(comment.timestamp, NORMAL_TIME_FORMAT)}</div>
+                  </div>
+                    <div className="body">{comment.body}</div>
+                  </div>
                 ))}
               </div>
             </div>
