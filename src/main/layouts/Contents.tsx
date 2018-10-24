8053bd50661c1aec32128bd3b7558473bbacf400
Richard <cyanyao@live.com>
2018-10-25 01:43:55 +0800

    update comments styles

--- src/main/layouts/Contents.tsx
+++ src/main/layouts/Contents.tsx
@@ -48,6 +48,12 @@ class Contents extends React.Component<Props> {
     article.content = article.content ? article.content : '';
     article.comments = article.comments ? article.comments : [];
 
+    const ThumbUp = () => (
+      <svg xmlns="http://www.w3.org/2000/svg" width="7" height="7" className="thumb-up">
+        <path d="M4.438 0c-.19.021-.34.149-.438.344-.13.26-1.101 2.185-1.281 2.375-.19.18-.439.281-.719.281v4.001h3.5c.21 0 .389-.133.469-.313 0 0 1.031-2.908 1.031-3.188 0-.28-.22-.5-.5-.5h-1.5c-.28 0-.5-.25-.5-.5s.389-1.574.469-1.844c.08-.27-.053-.545-.313-.625l-.219-.031zm-4.438 3v4h1v-4h-1z" id="thumb-up" />
+      </svg>
+    )
+
     return (
       <div className="contents">
         {loading
@@ -56,19 +62,27 @@ class Contents extends React.Component<Props> {
             <div className="article">
               <h1 className="title">{article.title}</h1>
               <div className="article-info">
-                <span>{article.author}</span>
                 <span>{formatDate(article.timestamp, SHORT_TIME_FORMAT)}</span>
+                <span>{t(article.category)}</span>
               </div>
               <div className="article-content"
                 dangerouslySetInnerHTML={{ __html: marked(article.content) }} />
               <div className="comments">
                 {article.comments.map((comment: Comments) => (
                   <div className="comment" key={comment.cid}>
-                  <div className="head">
-                    <div className="from">{comment.from}</div>
-                    <div className="time">{formatDate(comment.timestamp, NORMAL_TIME_FORMAT)}</div>
-                  </div>
-                    <div className="body">{comment.body}</div>
+                    <div className="avatar">avatar</div>
+                    <div className="main">
+                      <div className="head">
+                        <div className="from">{comment.from}</div>
+                        <div className="time">{formatDate(comment.timestamp, NORMAL_TIME_FORMAT)}</div>
+                      </div>
+                      <div className="body">{comment.body}</div>
+                      <div className="button">
+                        <button><ThumbUp /></button>
+                        <button>dislike</button>
+                        <button>{t('reply')}</button>
+                      </div>
+                    </div>
                   </div>
                 ))}
               </div>
