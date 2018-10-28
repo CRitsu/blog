c2b6469a1fa31f1986532ac279a6c2834855dcdf
Richard <cyanyao@live.com>
2018-10-29 00:01:45 +0800

    extract ThumbUp into single file and improve performance

--- src/main/layouts/Contents.tsx
+++ src/main/layouts/Contents.tsx
@@ -8,6 +8,7 @@ import { fetchArticle } from 'src/reducers/creators';
 import { checkArticle, formatDate } from 'src/utils';
 import { Comments, ContentsType, ReduxDispatch, Translate } from '../../types';
 import Loading from '../accessories/Loading';
+import ThumbUp from '../accessories/ThumbUp';
 
 
 // settings of markdown parser
@@ -28,12 +29,16 @@ interface Props extends ContentsType, Translate, ReduxDispatch {
 
 interface State {
   replyFlg: boolean[],
+  comment: string,
+  reply: string[]
 }
 
 
 class Contents extends React.Component<Props, State> {
 
   public state = {
+    comment: '',
+    reply: Array<string>(),
     replyFlg: Array<boolean>(),
   }
 
@@ -42,6 +47,9 @@ class Contents extends React.Component<Props, State> {
   public constructor(props: Props) {
     super(props);
     this.handleClickReplyButton = this.handleClickReplyButton.bind(this);
+    this.handleCommentChange = this.handleCommentChange.bind(this);
+    this.handleReplyChange = this.handleReplyChange.bind(this);
+    this.setRefObject = this.setRefObject.bind(this);
   }
 
   public componentDidMount() {
@@ -63,23 +71,70 @@ class Contents extends React.Component<Props, State> {
    * @param index index
    */
   public handleClickReplyButton(index: number) {
-    // get copy of flag array
-    const flg = this.state.replyFlg.slice();
-    // trigger flag
-    flg[index] = !flg[index];
-    // apply new flags
-    this.setState({ replyFlg: flg }, () => {
-      // set focus as a callback (setState is asynchronous)
-      const ref = this.commentsRef[index];
-      ref.focus();
-    });
+
+    return () => {
+      // get copy of flag array
+      const flg = this.state.replyFlg.slice();
+      // trigger flag
+      flg[index] = !flg[index];
+      // apply new flags
+      this.setState({ replyFlg: flg }, () => {
+        // set focus as a callback (setState is asynchronous)
+        const ref = this.commentsRef[index];
+        ref.focus();
+      });
+    }
+  }
+
+  /**
+   * Handler for comment input changed.
+   * @param e event object
+   */
+  public handleCommentChange(e: React.FormEvent) {
+    const target = e.target as HTMLTextAreaElement;
+    this.setState({ comment: target.value });
+  }
+
+  /**
+   * Handler for replies input changed.
+   * @param index index
+   */
+  public handleReplyChange(index: number) {
+    return (e: React.FormEvent) => {
+      const target = e.target as HTMLTextAreaElement;
+      const reply = this.state.reply.slice();
+      reply[index] = target.value;
+      this.setState({ reply });
+    }
+  }
+
+  /**
+   * Get row count by check how many times this \n was inputted.
+   * 
+   * Return 1 when str is not defined.
+   * @param str string
+   */
+  public getRowCount(str?: string) {
+    // check for safe
+    if (str === undefined) {
+      return 1;
+    }
+    const matches = str.match(/\n/g);
+    return matches ? matches.length + 1 : 1;
+  }
+
+  /**
+   * Reference object setter for focus textarea element.
+   * @param i index
+   */
+  public setRefObject(i: number) {
+    return (element: HTMLTextAreaElement) => this.commentsRef[i] = element;
   }
 
   public render() {
 
     const { t, loading } = this.props;
-    const { replyFlg } = this.state;
-    // const { replyFlg, commentsRef } = this.state;
+    const { replyFlg, comment, reply } = this.state;
 
     const article = checkArticle(this.props.article);
     article.content = article.content ? article.content : '';
@@ -90,19 +145,13 @@ class Contents extends React.Component<Props, State> {
       article.comments[i].replyFlg = !!replyFlg[i];
     }
 
-    const replyButtonClickHandlerWrapper = (i: number) => {
-      return () => this.handleClickReplyButton(i);
-    }
-
-    const setRefObject = (i: number) => {
-      return (element: HTMLTextAreaElement) => this.commentsRef[i] = element;
-    }
-
-    const ThumbUp = (props: { className?: string }) => (
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className={`thumb-up ${props.className}`}>
-        <path d="M1 21h4V9H1v12zm22-11c0-1.1-.9-2-2-2h-6.31l.95-4.57.03-.32c0-.41-.17-.79-.44-1.06L14.17 1 7.59 7.59C7.22 7.95 7 8.45 7 9v10c0 1.1.9 2 2 2h9c.83 0 1.54-.5 1.84-1.22l3.02-7.05c.09-.23.14-.47.14-.73v-1.91l-.01-.01L23 10z" />
-      </svg>
-    )
+    const {
+      handleClickReplyButton,
+      handleCommentChange,
+      handleReplyChange,
+      getRowCount,
+      setRefObject
+    } = this;
 
     return (
       <div className="contents">
@@ -110,46 +159,55 @@ class Contents extends React.Component<Props, State> {
           ? <Loading>{t('loading')}</Loading>
           : (
             <div className="article">
+
               <h1 className="title">{article.title}</h1>
+
               <div className="article-info">
                 <span>{formatDate(article.timestamp, SHORT_TIME_FORMAT)}</span>
                 <span>{t(article.category)}</span>
               </div>
+
               <div className="article-content"
                 dangerouslySetInnerHTML={{ __html: marked(article.content) }} />
+
               <div className="leave-comment">
                 <div className="avatar">avatar</div>
-                <textarea className="comment-input" placeholder={t('add a comment')} />
+                <textarea className="comment-input" placeholder={t('add a comment')}
+                  onChange={handleCommentChange} value={comment} rows={getRowCount(comment)} />
                 <button className="comment-submit">{t('comment')}</button>
               </div>
+
               <div className="comments">
-                {article.comments.map((comment: Comments, i: number) => (
-                  <div className="comment" key={comment.cid}>
+                {article.comments.map((c: Comments, i: number) => (
+                  <div className="comment" key={c.cid}>
                     <div className="avatar">avatar</div>
                     <div className="main">
                       <div className="head">
-                        <div className="from">{comment.from}</div>
-                        <div className="time">{formatDate(comment.timestamp, NORMAL_TIME_FORMAT)}</div>
+                        <div className="from">{c.from}</div>
+                        <div className="time">{formatDate(c.timestamp, NORMAL_TIME_FORMAT)}</div>
                       </div>
-                      <div className="body">{comment.body}</div>
+                      <div className="body">{c.body}</div>
                       <div className="buttons">
                         <button className="like active" title={t('like')}>
-                          <ThumbUp />1234
+                          <ThumbUp />123
                         </button>
                         <button className="dislike" title={t('dislike')}>
-                          <ThumbUp />1234
+                          <ThumbUp />123
                         </button>
-                        <button className="reply" onClick={replyButtonClickHandlerWrapper(i)}>{t('reply')}</button>
+                        <button className="reply" onClick={handleClickReplyButton(i)}>{t('reply')}</button>
                       </div>
-                      <div className={`reply-body ${comment.replyFlg ? 'active' : ''}`}>
+                      <div className={`reply-body ${c.replyFlg ? 'active' : ''}`}>
                         <div className="your-avatar">avatar</div>
-                        <textarea name={comment.cid} className="reply-input" ref={setRefObject(i)} placeholder={t('add a reply')}/>
+                        <textarea name={c.cid} className="reply-input" value={reply[i] ? reply[i] : ''}
+                          onChange={handleReplyChange(i)} ref={setRefObject(i)}
+                          rows={getRowCount(reply[i])} placeholder={t('add a reply')} />
                         <button className="reply-submit">{t('reply')}</button>
                       </div>
                     </div>
                   </div>
                 ))}
               </div>
+
             </div>
           )
         }
